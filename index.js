ae7d4b8dd96d060ed8559d717c171b52fa5df456
Miles Hamilton <mham7714@gmail.com>
2020-04-04 09:33:09 -0400

    Added conditionals

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const cardOne = "queen"
-const cardTwo = "queen"
-const cardThree = "king"
-const cardFour = "king"
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardOne);
+// card array
+const cards = ['queen', 'queen', 'king', 'king']
+const cardsInPlay = []
+
+// pushing the first card from the cards arr to cards in play
+const cardOne = cards[0]
+const playCardOne = cardsInPlay.push(cardOne)
+
+// pushing the second card
+const cardTwo = cards[2]
+const playCardTwo = cardsInPlay.push(cardTwo)
+// console.log("user flipped" + " " + cardsInPlay)
+
+if (cardsInPlay.length=== 2) {
+    console.log(cardsInPlay)
+} if (cardsInPlay[0]===cardsInPlay[1]) {
+    alert("you found a match!!")
+} else {
+    alert ("gg no re")
+}

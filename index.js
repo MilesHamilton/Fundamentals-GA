ee4f95d163dea912c3ee2d8b17ea47fc63e51beb
Miles Hamilton <mham7714@gmail.com>
2020-04-05 20:29:12 -0400

    added functions and some logic

--- index.js
+++ index.js
@@ -3,19 +3,27 @@
 const cards = ['queen', 'queen', 'king', 'king']
 const cardsInPlay = []
 
-// pushing the first card from the cards arr to cards in play
-const cardOne = cards[0]
-const playCardOne = cardsInPlay.push(cardOne)
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+}
 
-// pushing the second card
-const cardTwo = cards[2]
-const playCardTwo = cardsInPlay.push(cardTwo)
-// console.log("user flipped" + " " + cardsInPlay)
+function flipCard(cardId) {
+    cardsInPlay.push(cards[cardId])
+
 
 if (cardsInPlay.length=== 2) {
-    console.log(cardsInPlay)
 } if (cardsInPlay[0]===cardsInPlay[1]) {
     alert("you found a match!!")
 } else {
     alert ("gg no re")
+    
 }
+console.log(checkForMatch())
+console.log("User Flipped" + " "+ cards[cardId])
+};
+console.log(flipCard(1))
+

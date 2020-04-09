2082456d3f19a372b6fd9f008ea9cda0bbdefe08
Miles Hamilton <mham7714@gmail.com>
2020-04-09 10:46:02 -0400

    Finished up Logic and created Deliverable game

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
 
 // card array
-const cards = ['queen', 'queen', 'king', 'king']
+const cards = [
+    {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: "images/king-of-hearts.png"
+
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: "images/king-of-diamonds.png"
+    }];
+    
 const cardsInPlay = []
 
-function checkForMatch() {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
-      } else {
-        console.log("Sorry, try again.");
-      }
-}
+function createBoard() {
+    for (let i = 0; i < cards.length; i++) {
+        const cardElement = document.createElement('img')
+        cardElement.setAttribute('src', "images/back.png")
+        cardElement.setAttribute('data-id', i)
+        cardElement.addEventListener('click', flipCard)
+        const gameBoard = document.getElementById('gameBoard')
+        gameBoard.appendChild(cardElement)
+    };
+};
 
-function flipCard(cardId) {
-    cardsInPlay.push(cards[cardId])
 
+// creating the events once the cards are flipped
+function flipCard (){
+    const cardId = this.getAttribute('data-id')
+    this.setAttribute('src', cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank)
 
 if (cardsInPlay.length=== 2) {
-} if (cardsInPlay[0]===cardsInPlay[1]) {
+    if (cardsInPlay[0]===cardsInPlay[1]) {
     alert("you found a match!!")
 } else {
-    alert ("gg no re")
-    
-}
-console.log(checkForMatch())
-console.log("User Flipped" + " "+ cards[cardId])
+    alert ("Wrong, sorry")
+    }};
+    console.log(cards[cardId].suit)
+    console.log(cards[cardId].cardImage)
+console.log("User Flipped" + " "+ cards[cardId].rank)
 };
-console.log(flipCard(1))
 
+
+// // displaying win or lose
+// function checkForMatch() {
+//     if (cardsInPlay[0] === cardsInPlay[1]) {
+//         console.log("You found a match!");
+//       } else {
+//         console.log("Sorry, try again.");
+//       }
+//     };
+
+// using DOM to set the initial board 
+
+createBoard()
